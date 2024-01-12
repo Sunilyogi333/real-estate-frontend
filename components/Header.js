da0d170641062600b317eda7db0498cc3ae69300
Sunilyogi333 <sunil.211443@ncit.edu.np>
2024-01-13 00:58:55 +0545

    feat: menu buttton added & items component updated

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,13 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import serenity from "../public/Images/Icons/serenity.png";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <>
-      <header className="flex h-16 bg-color-white backdrop-blur-xl w-full fixed left-0 top-0">
-        <div className="flex justify-between items-center w-full px-24 py-3">
+      <header className="flex h-16 bg-white bg-opacity-30 backdrop-blur-xl w-full fixed left-0 top-0">
+        <div className="flex justify-between items-center w-full px-8 lg:px-24 py-3">
           <div className="flex gap-5 items-center">
             <div className="flex gap-2">
               <Image src={serenity} alt="" />
@@ -20,14 +21,7 @@ const Header = () => {
               sell
             </Link>
           </div>
-          <div className="flex items-center text-center">
-            <button className="border-2 border-green-500 cursor-pointer text-blue-500 font-semibold text-base py-2 px-4 mr-4 rounded-md">
-              Login
-            </button>
-            <button className="bg-gray-300 border-transparent hover:border-gray-500 cursor-pointer text-white font-semibold text-base py-2 px-4 rounded-md">
-              Sign up
-            </button>
-          </div>
+          <Navigation />
         </div>
       </header>
       <hr className="mt-16 mb-3" />
