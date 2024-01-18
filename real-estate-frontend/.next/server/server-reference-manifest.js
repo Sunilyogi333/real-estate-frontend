1d6b76ba86aba80c4fc536092176c23d485c8a89
Sunilyogi333 <sunil.211443@ncit.edu.np>
2024-01-18 23:57:54 +0545

    .gitignore

--- real-estate-frontend/.next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"n+aQ7sbmjWDAKkWjMV9xwRjSoA6yZ0mHRP8btnFunRo=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9CUJeqvaRJeWQ1ixrF3H6t/X0wV6PTGcYx+tuvKs+QI=\"\n}"
->>>>>>> pragya
