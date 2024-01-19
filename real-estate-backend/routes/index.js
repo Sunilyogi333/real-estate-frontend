dbaea639415796a806c4982527c1a74d70d57f51
Sunilyogi333 <sunil.211443@ncit.edu.np>
2024-01-19 12:43:37 +0545

    password hashing

--- real-estate-backend/routes/index.js
+++ real-estate-backend/routes/index.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var con = require('./connection');
-const cors = require('cors');
+var con = require("./connection");
+const cors = require("cors");
 const upload = require("./multer");
+const bycrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const secretKey = "secretKey";
+const salt = 10;
 
 router.use(cors());
 router.use(express.json());
@@ -25,8 +27,8 @@ const verifyJwt = (req, res, next) => {
   });
 };
 
-router.get('/',verifyJwt ,function(req, res, next) {
-  res.json({message: 'Authorized', success: true});
+router.get("/", verifyJwt, function (req, res, next) {
+  res.json({ message: "Authorized", success: true });
 });
 
 router.get("/get", function (req, res) {
@@ -35,56 +37,41 @@ router.get("/get", function (req, res) {
     res.send(result);
   });
 });
+
 //signup
 router.post("/signup", function (req, res) {
-
-  console.log(req.body , "body");
-  var name = req.body.name;
-  var email = req.body.email;
-  var password = req.body.password;
-
-  var sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-  console.log("🚀 ~ sql:", sql)
-
-  con.query(sql, [name, email, password], function (err, result) {
-    if (err) {
-      console.log('sunil')
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-      return;
-    }
-
-    res.status(200).json({ message: 'User successfully registered', success: true });
+  bycrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+    if (err) return res.json({ Error: "Error for hassing password" });
+    const values = [req.body.name, req.body.email, hash];
+    const sql = "INSERT INTO users (username, email, password) VALUES (?)";
+    con.query(sql, [values], function (err, result) {
+      if (err) return res.json({ Error: "Error for inserting data" });
+      return res.json({ message: "User succesfully registerd", success: true });
+    });
   });
 });
 
 //login
 router.post("/login", function (req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-  console.log(req.body , "body")
-  var sql = "SELECT * FROM users WHERE email = ? AND password = ?";
-  console.log("🚀 ~ sql:", sql)
-  con.query(sql, [email, password], function (err, result) {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-      return;
-    }
-
-    if (result.length === 0) {
-      res.status(401).json({ message: 'Invalid credentials', success: false  });
-      return;
-    }
-    console.log(result)
-    const userId = result[0].userId;
-    const token = jwt.sign({ userId }, secretKey);
-    return res.json({success: true, token });
+  const sql = "SELECT * FROM users WHERE email = ?";
+  con.query(sql, [req.body.email], (err, result) => {
+    if (err) return res.json({ Error: "Login error in server" });
+    if (result.length === 0) return res.json({ Error: "User not found" });
+    bycrypt.compare(req.body.password.toString(),result[0].password,(err, passwordMatch) => {
+        if (err) return res.json({ Error: "Login error in server" });
+        if (passwordMatch) {
+          const userId = result[0].userId;
+          const token = jwt.sign({ userId }, secretKey);
+          return res.json({ message: "Login success", success: true, token });
+        } else {
+          return res.json({ Error: "Password incorrect" });
+        }
+      }
+    );
   });
-}
-);
+});
 
-//add Properties 
+//add Properties
 //Property table includes, name, location,price, bedrooms, bathrooms, squareArea, date, type, yearBuilt, city
 router.post("/addProperty", function (req, res) {
   var userId = req.body.userId;
@@ -98,19 +85,36 @@ router.post("/addProperty", function (req, res) {
   var type = req.body.type;
   var yearBuilt = req.body.yearBuilt;
   var city = req.body.city;
-  var sql = "INSERT INTO property (name, location, price, bedrooms, bathrooms, squareArea, date, type, yearBuilt, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  console.log("🚀 ~ sql:", sql)
-  con.query(sql, [name, location, price, bedrooms, bathrooms, squareArea, date, type, yearBuilt, city], function (err, result) {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-      return;
+  var sql =
+    "INSERT INTO property (name, location, price, bedrooms, bathrooms, squareArea, date, type, yearBuilt, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  console.log("🚀 ~ sql:", sql);
+  con.query(
+    sql,
+    [
+      userId,
+      name,
+      location,
+      price,
+      bedrooms,
+      bathrooms,
+      squareArea,
+      date,
+      type,
+      yearBuilt,
+      city,
+    ],
+    function (err, result) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server Error" });
+        return;
+      }
+
+      res
+        .status(200)
+        .json({ message: "Property successfully added", success: true });
     }
-
-    res.status(200).json({ message: 'Property successfully added', success: true });
-  });
-}
-);
-
+  );
+});
 
 module.exports = router;
