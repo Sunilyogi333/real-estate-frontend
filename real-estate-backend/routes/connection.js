871b1452c67f8f6bf1cfcf89c1cf2e441808a04d
Sunilyogi333 <sunilnath0109@gmail.com>
2024-02-13 18:49:37 +0545

    cloud connection

--- real-estate-backend/routes/connection.js
+++ real-estate-backend/routes/connection.js
@@ -1,25 +1,17 @@
 var mysql = require('mysql');
-var con = mysql.createPool({
-    host: process.env.host,
-    user: process.env.user,
+const con = mysql.createPool({
+    host: process.env.host, 
+    user: process.env.username, 
     password: process.env.password,
     database: process.env.database,
-    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-pool.getConnection((err, conn) => {
+
+con.getConnection((err, conn) => {
     if(err) console.log(err)
     console.log("Connected successfully")
 })
 
 module.exports = con.promise()
-
-// con.connect(function (err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// }
-// );
-
-// module.exports = con;
