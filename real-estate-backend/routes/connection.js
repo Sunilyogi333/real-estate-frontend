e8d2f1191b52324fb4432b6b98cb83238d01ff25
Sunilyogi333 <sunilnath0109@gmail.com>
2024-03-18 22:29:25 +0545

    dotenv added

--- real-estate-backend/routes/connection.js
+++ real-estate-backend/routes/connection.js
@@ -1,10 +1,13 @@
 var mysql = require('mysql');
+const dotenv = require("dotenv");
+dotenv.config();
+
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'sunil',
-    password: "Sunil@333",
-    database: 'serenity',
-    port: 3306,
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    port: process.env.port,
 });
 con.connect(function (err) {
     if (err) throw err;
